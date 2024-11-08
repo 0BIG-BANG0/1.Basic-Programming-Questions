ecf55da3b798ca0b3ebe8eb5433f8f25500ae1c9
0BIG-BANG0 <96254268+0BIG-BANG0@users.noreply.github.com>
2024-11-08 12:15:29 +0530

    3 completed

--- 3.RemoveDuplicate.js
+++ 3.RemoveDuplicate.js
@@ -1,14 +1,14 @@
 function removeDuplicate(arr) {
-  let count = 0;
-  let elem = 0;
-  let index;
-  for (let i = 0; i < arr.length; i++) {
+    let elem = 0;
+    let index;
+    for (let i = 0; i < arr.length; i++) {
+      let count = 0;
     for (let j = 0; j < arr.length; j++) {
-      if (arr[i] == arr[j]) {
+      if (arr[i] === arr[j]) {
         count++;
         if (count > 1) {
-          elem = arr[i];
-          index = i;
+          elem = arr[j];
+          index = j;
         }
       }
     }
@@ -16,7 +16,11 @@ function removeDuplicate(arr) {
   let arrResult = arr.splice(index, 1);
   return arr;
 }
+function removeDuplicateInbuilt(arr){
+    return arr.filter((item,index)=> arr.indexOf(item) === index)
+}
 
 let arr = [1, 2, 1, 3, 4, 5, 6];
-let result = removeDuplicate(arr);
+// let result = removeDuplicate(arr);
+let result = removeDuplicateInbuilt(arr);
 console.log(result);
